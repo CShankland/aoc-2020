797ca55d1c505edc2902bfc18f4c6d704c057567
cshankland <cshankland@plateaugames.com>
2020-12-03 11:55:15 -0500

    Day 3; All parts

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,56 @@
-#[macro_use]
-extern crate lazy_static;
+use std::fs;
+use std::str::Lines;
 
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-use regex::Regex;
-use std::num;
-
-enum AocError
+pub struct TobogganPath<'a>
 {
-    IoError(io::Error),
-    ParseError(num::ParseIntError),
+    lines: Lines<'a>,
+    offset: usize,
+    skip: usize,
+    right: usize,
+    down: usize,
 }
 
-impl From<io::Error> for AocError
+impl Iterator for TobogganPath<'_>
 {
-    fn from(error: io::Error) -> Self
+    type Item = char;
+
+    fn next(&mut self) -> Option<char>
     {
-        AocError::IoError(error)
+        let result = self.lines.nth(self.skip)
+            .map_or(None, |l| l.chars().cycle().nth(self.offset));
+        self.offset += self.right;
+        self.skip = self.down - 1;
+        result
     }
 }
 
-impl From<num::ParseIntError> for AocError
+fn compute_toboggan_path(input: &String, right: usize, down: usize) -> TobogganPath
 {
-    fn from(error: num::ParseIntError) -> Self
-    {
-        AocError::ParseError(error)
-    }
+    TobogganPath{ lines: input.lines(), offset: 0, skip: 0, right, down }
 }
 
 const DATA_PATH: &str = "C:\\Development\\aoc-2020\\data";
 
-struct PasswordEntry
+fn is_tree(terrain: &char) -> bool
 {
-    password: String,
-    required: char,
-    first_index: usize,
-    second_index: usize
+    *terrain == '#'
+}
+
+fn count_trees(input: &String, right: usize, down: usize) -> usize
+{
+    compute_toboggan_path(&input, right, down)
+        .filter(is_tree)
+        .count()
 }
 
 fn main() {
-    let valid_passwords = read_lines(format!("{}\\{}", DATA_PATH, "day2.txt"))
-        .unwrap()
-        .map(|line| parse_password_entry(&line.unwrap()))
-        .filter_map(Result::ok)
-        .map(|entry| verify_password(&entry))
-        .fold(0, |acc, x| acc + x as i32);
+    let input = fs::read_to_string(format!("{}\\{}", DATA_PATH, "day3.txt")).unwrap();
+    let trees_hit =
+        count_trees(&input, 1, 1) *
+        count_trees(&input, 3, 1) *
+        count_trees(&input, 5, 1) *
+        count_trees(&input, 7, 1) *
+        count_trees(&input, 1, 2);
 
-    println!("Valid passwords: {}", valid_passwords);
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn parse_password_entry(input: &String) -> Result<PasswordEntry, AocError>
-{
-    lazy_static!
-    {
-        // 2-9 c: ccccccccc
-        static ref RE: Regex = Regex::new(r"(?P<min>\d+)-(?P<max>\d+) (?P<req>\w): (?P<pwd>\w+)").unwrap();
-    }
-
-    let parse_result = RE.captures(input).unwrap();
-    let first_index = parse_result["min"].to_string().parse::<usize>()?;
-    let second_index = parse_result["max"].to_string().parse::<usize>()?;
-
-    Ok(PasswordEntry
-    {
-        password: parse_result["pwd"].to_string(),
-        required: parse_result["req"].to_string().chars().nth(0).unwrap(),
-        first_index,
-        second_index,
-    })
-}
-
-fn verify_password(password_data: &PasswordEntry) -> bool
-{
-    let has_first = password_data.password.chars().nth(password_data.first_index - 1).map_or(false, |c| c == password_data.required);
-    let has_second = password_data.password.chars().nth(password_data.second_index - 1).map_or(false, |c| c == password_data.required);
-
-    return has_first ^ has_second;
+    println!("Trees hit: {}", trees_hit);
 }
