9d19808cec195602d1f6c6582075b178ec33e8bd
cshankland <cshankland@plateaugames.com>
2020-12-06 15:20:29 -0500

    Day 5

--- src/main.rs
+++ src/main.rs
@@ -1,229 +1,89 @@
-#[macro_use]
-extern crate lazy_static;
-
 use std::fs;
-use regex::Regex;
 use std::str::Lines;
-use std::str::FromStr;
-use std::num::ParseIntError;
-use crate::AocError::ParseError;
 
-pub enum AocError
-{
-    ParseIntError(ParseIntError),
-    ParseError,
-}
-
-impl From<ParseIntError> for AocError
-{
-    fn from(error: ParseIntError) -> Self
-    {
-        AocError::ParseIntError(error)
-    }
-}
-
-pub struct PassportParser<'a>
+pub struct SeatParser<'a>
 {
     lines: Lines<'a>
 }
 
-pub struct Height
+pub struct SeatData
 {
-    units: String,
-    value: i32
+    row: i32,
+    column: i32
 }
 
-fn invalid_height() -> Height
+fn parse_to_seat(input: &String) -> SeatParser
 {
-    Height { units: String::from("none"), value: 0 }
+    SeatParser{ lines: input.lines() }
 }
 
-impl FromStr for Height
+fn compute_bit(letter: char) -> i32
 {
-    type Err = AocError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err>
+    match letter
     {
-        lazy_static!
-        {
-            static ref RE: Regex = Regex::new(r"(?P<value>\d+)(?P<units>.+)").unwrap();
-        }
-
-        if let Some(captures) = RE.captures(s)
-        {
-            let value = captures["value"].to_string().parse::<i32>()?;
-            let units = captures["units"].to_string();
-
-            Ok(Height{ units, value })
-        }
-        else
-        {
-            Err(ParseError)
-        }
+        'B' => 1,
+        'R' => 1,
+        _ => 0
     }
 }
 
-pub struct PassportData
+impl Iterator for SeatParser<'_>
 {
-    byr: i32,
-    iyr: i32,
-    eyr: i32,
-    hgt: Height,
-    hcl: String,
-    ecl: String,
-    pid: String,
-    cid: String,
-}
+    type Item = SeatData;
 
-fn parse_to_passports(input: &String) -> PassportParser
-{
-    PassportParser{ lines: input.lines() }
-}
-
-fn populate_passport_field(passport: &mut PassportData, field_data: &str)
-{
-    if let Some(colon_index) = field_data.find(":")
+    fn next(&mut self) -> Option<SeatData>
     {
-        let field = &field_data[..colon_index];
-        let value: String = field_data[colon_index+1..].to_string();
-
-        println!("Parsing field {} : {}", field, value);
-
-        match field
-        {
-            "byr" => passport.byr = value.parse::<i32>().unwrap_or(0),
-            "iyr" => passport.iyr = value.parse::<i32>().unwrap_or(0),
-            "eyr" => passport.eyr = value.parse::<i32>().unwrap_or(0),
-            "hgt" => passport.hgt = value.parse::<Height>().unwrap_or(invalid_height()),
-            "hcl" => passport.hcl = value,
-            "ecl" => passport.ecl = value,
-            "pid" => passport.pid = value,
-            "cid" => passport.cid = value,
-
-            _ => println!("Unknown field {}", field)
-        }
-    }
-    else
-    {
-        println!("No colon in data {}", field_data);
+        self.lines.next()
+            .map(|line| line.chars().fold(0, |acc, c| (acc << 1) + compute_bit(c)))
+            .map(|value| SeatData{ row: value >> 3, column: value & 0x7 })
     }
 }
 
-impl Iterator for PassportParser<'_>
+fn compute_seat_id(data: &SeatData) -> i32
 {
-    type Item = PassportData;
-
-    fn next(&mut self) -> Option<PassportData>
-    {
-        let mut maybe_line = self.lines.next();
-        if maybe_line.is_none()
-        {
-            return None;
-        }
-
-        let mut result: PassportData = PassportData {
-            byr: 0,
-            iyr: 0,
-            eyr: 0,
-            hgt: invalid_height(),
-            hcl: String::from(""),
-            ecl: String::from(""),
-            pid: String::from(""),
-            cid: String::from("")
-        };
-
-        while let Some(line) = maybe_line
-        {
-            println!("Read line {}", line);
-
-            if line.is_empty()
-            {
-                println!("Passport data complete.");
-                break;
-            }
-
-            line.split_ascii_whitespace()
-                .for_each(|key_value| populate_passport_field(&mut result, key_value));
-
-            maybe_line = self.lines.next();
-        }
-
-        return Some(result);
-    }
-}
-
-fn between(value: i32, min: i32, max: i32) -> bool
-{
-    return value >= min && value <= max;
-}
-
-fn valid_height(height: &Height) -> bool
-{
-    match height.units.as_ref()
-    {
-        "cm" => between(height.value, 150, 193),
-        "in" => between(height.value, 59, 76),
-        _ => false
-    }
-}
-
-fn valid_hair_color(value: &str) -> bool
-{
-    lazy_static!
-    {
-        static ref RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
-    }
-
-    RE.is_match(value)
-}
-
-fn valid_eye_color(value: &str) -> bool
-{
-    match value
-    {
-        "amb" => true,
-        "blu" => true,
-        "brn" => true,
-        "gry" => true,
-        "grn" => true,
-        "hzl" => true,
-        "oth" => true,
-        _ => false
-    }
-}
-
-fn valid_passport_id(value: &str) -> bool
-{
-    lazy_static!
-    {
-        static ref RE: Regex = Regex::new(r"^\d{9}$").unwrap();
-    }
-
-    RE.is_match(value)
-}
-
-fn is_valid(passport: &PassportData) -> bool
-{
-    let mut valid: bool = true;
-
-    valid = valid && between(passport.byr, 1920, 2002);
-    valid = valid && between(passport.iyr, 2010, 2020);
-    valid = valid && between(passport.eyr, 2020, 2030);
-    valid = valid && valid_height(&passport.hgt);
-    valid = valid && valid_hair_color(&passport.hcl);
-    valid = valid && valid_eye_color(&passport.ecl);
-    valid = valid && valid_passport_id(&passport.pid);
-
-    return valid;
+    data.row * 8 + data.column
 }
 
 const DATA_PATH: &str = "C:\\Development\\aoc-2020\\data";
 
 fn main() {
-    let input = fs::read_to_string(format!("{}\\{}", DATA_PATH, "day4.txt")).unwrap();
-    let valid_passports = parse_to_passports(&input)
-        .filter(is_valid)
-        .count();
+    let input = fs::read_to_string(format!("{}\\{}", DATA_PATH, "day5.txt")).unwrap();
+    let mut seats: Vec<i32> = parse_to_seat(&input)
+        .map(|seat| compute_seat_id(&seat))
+        .collect();
+    seats.sort();
+    let mut seat_iter = seats.iter().peekable();
+    while let Some(seat) = seat_iter.next()
+    {
+        if let Some(next_seat) = seat_iter.peek()
+        {
+            if **next_seat == *seat + 2
+            {
+                println!("Available seat: {}", *seat + 1);
+            }
+        }
+    }
+}
 
-    println!("Valid passports: {}", valid_passports);
+#[test]
+fn check_parser()
+{
+    let input = String::from("BFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL");
+    let seats: Vec<SeatData> = parse_to_seat(&input).collect();
+    assert_eq!(3, seats.len());
+
+    let seat_1 = &seats[0];
+    assert_eq!(seat_1.row, 70);
+    assert_eq!(seat_1.column, 7);
+    assert_eq!(compute_seat_id(seat_1), 567);
+
+    let seat_2 = &seats[1];
+    assert_eq!(seat_2.row, 14);
+    assert_eq!(seat_2.column, 7);
+    assert_eq!(compute_seat_id(seat_2), 119);
+
+    let seat_3 = &seats[2];
+    assert_eq!(seat_3.row, 102);
+    assert_eq!(seat_3.column, 4);
+    assert_eq!(compute_seat_id(seat_3), 820);
 }
