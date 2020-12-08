5e10be2c734559027336911e2c439f85c3f91261
cshankland <cshankland@plateaugames.com>
2020-12-08 09:36:54 -0500

    Day 8, part 1

--- src/main.rs
+++ src/main.rs
@@ -4,130 +4,152 @@ extern crate lazy_static;
 use std::fs;
 use regex::Regex;
 use std::str::Lines;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
-pub struct BaggageRuleParser<'a>
+#[derive(Debug)]
+#[derive(PartialEq)]
+enum OpCode
+{
+    Unknown,
+    Noop,
+    Accumulate,
+    Jump,
+}
+
+struct Instruction
+{
+    operation: OpCode,
+    value: i32
+}
+
+struct Program
+{
+    instructions: Vec<Instruction>,
+    instruction_pointer: i32,
+    accumulator: i32,
+}
+
+struct AocParser<'a>
 {
     lines: Lines<'a>
 }
 
-pub struct BagCount
+impl Iterator for AocParser<'_>
 {
-    bag_type: String,
-    count: usize
+    type Item = Instruction;
+
+    fn next(&mut self) -> Option<Instruction>
+    {
+        self.lines.next()
+            .map(|line| line.trim())
+            .map(parse_instruction)
+    }
 }
 
-pub struct BaggageRule
+fn parse_instruction(input: &str) -> Instruction
 {
-    outer_bag: String,
-    contents: Vec<BagCount>
-}
-
-pub struct BaggageContainment
-{
-    rule: usize,
-    contained_in: Vec<usize>
-}
-
-fn parse_baggage_rule(input: &str) -> BaggageRule
-{
-    let mut rule = BaggageRule{ outer_bag: String::from(""), contents: Vec::new() };
-
     lazy_static!
     {
-        static ref RE: Regex = Regex::new(r",? |\.").unwrap();
+        static ref RE: Regex = Regex::new(r"(\w+) ([+-]\d+)").unwrap();
     }
 
-    let mut rule_iter = RE.split(input);
-    rule.outer_bag = (&mut rule_iter).take(2).collect::<Vec<&str>>().join(" ");
-
-    // contain
-    (&mut rule_iter).next();
-    (&mut rule_iter).next();
-
-    while let Some(count) = (&mut rule_iter).next()
+    let captures = RE.captures(input).unwrap();
+    let operation = match captures.get(1).unwrap().as_str()
     {
-        match count
+        "nop" => OpCode::Noop,
+        "acc" => OpCode::Accumulate,
+        "jmp" => OpCode::Jump,
+        _ => OpCode::Unknown
+    };
+    let value = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
+
+    return Instruction{ operation, value };
+}
+
+fn parse_input(input: &String) -> AocParser { AocParser{ lines: input.lines() } }
+
+fn run_program(instructions: Vec<Instruction>, evaluation_callback: &mut dyn FnMut(&Program) -> bool) -> i32
+{
+    let mut program = Program{ instructions, instruction_pointer: 0, accumulator: 0 };
+
+    while evaluation_callback(&program)
+    {
+        let instruction: &Instruction = &program.instructions[program.instruction_pointer as usize];
+        match instruction.operation
         {
-            "" => break,
-            "no" => break,
-            _ =>
+            OpCode::Noop => program.instruction_pointer += 1,
+            OpCode::Accumulate =>
                 {
-                    let bag_type: String = (&mut rule_iter).take(2).collect::<Vec<&str>>().join(" ");
-                    rule.contents.push(BagCount{ bag_type , count: count.parse::<usize>().unwrap() });
-                    (&mut rule_iter).next();
-                }
+                    program.accumulator += instruction.value;
+                    program.instruction_pointer += 1;
+                },
+            OpCode::Jump =>
+                {
+                    program.instruction_pointer += instruction.value;
+                },
+            _ => panic!("Unknown instruction")
         }
     }
 
-    return rule;
+    return program.accumulator;
 }
-
-impl Iterator for BaggageRuleParser<'_>
-{
-    type Item = BaggageRule;
-
-    fn next(&mut self) -> Option<BaggageRule>
-    {
-        self.lines.next()
-            .map(parse_baggage_rule)
-    }
-}
-
-fn parse_to_baggage_rules(input: &String) -> BaggageRuleParser { BaggageRuleParser{ lines: input.lines() } }
-
 
 const DATA_PATH: &str = "C:\\Development\\aoc-2020\\data";
 
 fn main() {
-    let input = fs::read_to_string(format!("{}\\{}", DATA_PATH, "day7.txt")).unwrap();
-    let rules: Vec<BaggageRule> = parse_to_baggage_rules(&input)
-        .collect();
+    let instructions: Vec<Instruction> = fs::read_to_string(format!("{}\\{}", DATA_PATH, "day8.txt"))
+        .map(|input| parse_input(&input).collect())
+        .unwrap();
 
-    let rule_size = rules.len();
-    let mut rules_lookup = HashMap::new();
-    for idx in 0..rule_size
+    let mut evaluated_instructions: HashSet<i32> = HashSet::new();
+    let mut prevent_loop = |program: &Program|
     {
-        rules_lookup.insert(rules[idx].outer_bag.clone(), BaggageContainment{ rule: idx, contained_in: Vec::new() });
-    }
-
-    for idx in 0..rule_size
-    {
-        let rule: &BaggageRule = &rules[idx];
-        for item in &rule.contents
+        if evaluated_instructions.contains(&program.instruction_pointer)
         {
-            rules_lookup.get_mut(&item.bag_type).unwrap().contained_in.push(idx);
+            return false;
         }
-    }
 
-    let mut bag_count: usize = 0;
-    let mut search: Vec<(usize, usize)> = vec![(rules_lookup["shiny gold"].rule, 1)];
-    while let Some(next) = search.pop()
-    {
-        rules[next.0].contents.iter()
-            .for_each(|item|
-                {
-                    bag_count += item.count * next.1;
-                    search.push((rules_lookup[&item.bag_type].rule, next.1 * item.count));
-                });
-    }
+        evaluated_instructions.insert(program.instruction_pointer);
+        return true;
+    };
 
-    println!("{} total bags are inside a single shiny gold bag", bag_count);
+    let result = run_program(instructions, &mut prevent_loop);
+    println!("Accumulator ended at {}", result);
 }
 
 #[test]
 fn check_parser()
 {
-    let input = String::from("light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags.");
-    let rules: Vec<BaggageRule> = parse_to_baggage_rules(&input).collect();
-    assert_eq!(rules.len(), 9);
+    let input = String::from(r#"nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6"#);
 
-    let rule_0 = &rules[0];
-    assert_eq!(rule_0.outer_bag, "light red");
-    assert_eq!(rule_0.contents.len(), 2);
-    assert_eq!(rule_0.contents[0].bag_type, "bright white");
-    assert_eq!(rule_0.contents[0].count, 1);
-    assert_eq!(rule_0.contents[1].bag_type, "muted yellow");
-    assert_eq!(rule_0.contents[1].count, 2);
+    let instructions: Vec<Instruction> = parse_input(&input).collect();
+    assert_eq!(instructions.len(), 9);
+
+    let item_0 = &instructions[0];
+    assert_eq!(item_0.operation, OpCode::Noop);
+    assert_eq!(item_0.value, 0);
+
+    let item_1 = &instructions[1];
+    assert_eq!(item_1.operation, OpCode::Accumulate);
+    assert_eq!(item_1.value, 1);
+
+    let item_2 = &instructions[2];
+    assert_eq!(item_2.operation, OpCode::Jump);
+    assert_eq!(item_2.value, 4);
+
+    let item_3 = &instructions[3];
+    assert_eq!(item_3.operation, OpCode::Accumulate);
+    assert_eq!(item_3.value, 3);
+
+    let item_4 = &instructions[4];
+    assert_eq!(item_4.operation, OpCode::Jump);
+    assert_eq!(item_4.value, -3);
 }
